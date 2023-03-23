f04164bfe44766222e1639b2c89b37f4d77c7681
M Inzamam Malik <malikasinger1@gmail.com>
2023-03-23 16:41:43 +0500

    working state

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-# importing libraries
-from flask import Flask, session, render_template, request, redirect
-import os  
+from flask import Flask, render_template
 
 
-#  How to run flask program reference: https://flask.palletsprojects.com/en/2.2.x/quickstart/#:~:text=To%20run%20the%20application%2C%20use,with%20the%20%2D%2Dapp%20option.&text=As%20a%20shortcut%2C%20if%20the,Line%20Interface%20for%20more%20details
 app = Flask(__name__)
-
-app.secret_key = 'secret'
-
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -23,15 +17,5 @@ def edit():
 def hello():
     return render_template('classifier.html')
 
-
-# # In[ ]:
-# if __name__ == "__main__":
-#     app.debug = True
-#     app.run()
-# else:
-#     gunicorn_app = app()
-# # https://stackoverflow.com/a/51397334
-
-PORT = os.environ.get('PORT', 3000)
-
-app.run(port=PORT)
+if __name__ == '__main__':
+    app.run()
